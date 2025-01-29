b449b4afcbf9700bc9421d884bd18b3d45e4daf0
Siddique Abdul <szuber558@gmail.com>
2025-01-29 15:31:43 +0530

    deploy5

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
-const port = env.PORT;
+const port = env.PORT || 5000;
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Mongoose connected");
     app.listen(port, () => {
-      console.log(`Server started successfully on port http://0.0.0.0:${port}`);
+      console.log(
+        `Server started successfully on port http://localhost:${port}`
+      );
     });
   })
   .catch(console.error);
